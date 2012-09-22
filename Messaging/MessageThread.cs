e51dd1e0ca8faab50a191c0a81fa1c0cc2b4837b
つーさ <tu3+github@tu3.jp>
2012-09-22 18:26:50 +0900

    あると何かと便利な、Win32メッセージコールバックを受け取れるスレッドとウィンドウ

---
+++ Messaging/MessageThread.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Tsukikage.Windows.Messaging
+{
+    /// <summary>
+    /// Win32 Message Loop Thread for win32 multimedia API callbacks
+    /// Win32 メッセージループスレッドを作って、マルチメディア系 Win32 APIからのコールバックを受け取る
+    /// </summary>
+    [SuppressUnmanagedCodeSecurity]
+    public sealed class MessageThread : IDisposable, IMessageFilter
+    {
+        Thread thread;
+
+        /// <summary>
+        /// Native Thread ID
+        /// スレッドID
+        /// </summary>
+        public int Win32ThreadID { get; private set; }
+
+        /// <summary>
+        /// Install your message handler to this.
+        /// メッセージハンドラをこれに組み込む
+        /// </summary>
+        public Dictionary<int, CallbackDelegate> MessageHandlers { get; private set; }
+
+        public delegate void CallbackDelegate(Message m);
+
+        /// <summary>
+        /// Create new message loop thread.
+        /// 新しいメッセージループスレッドを作る
+        /// </summary>
+        public MessageThread()
+        {
+            MessageHandlers = new Dictionary<int, CallbackDelegate>();
+            using (ManualResetEvent initialized = new ManualResetEvent(false))
+            {
+                thread = new Thread(delegate()
+                {
+                    Application.AddMessageFilter(this);
+#pragma warning disable
+                    Win32ThreadID = AppDomain.GetCurrentThreadId();
+#pragma warning enable
+                    initialized.Set();
+                    Application.Run();
+                });
+                thread.Start();
+                initialized.WaitOne();
+            }
+        }
+
+        /// <summary>
+        /// Post message to the thread.
+        /// スレッドにメッセージを送る
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        public void PostMessage(int msg, IntPtr wParam, IntPtr lParam)
+        {
+            PostThreadMessage(Win32ThreadID, msg, wParam, lParam);
+        }
+
+        /// <summary>
+        /// Exit thread by posting WM_QUIT and release all resources.
+        /// WM_QUITを送ってスレッドを終了し、リソースを解放する。
+        /// </summary>
+        public void Dispose()
+        {
+            if (thread != null)
+            {
+                const int WM_QUIT = 0x0012;
+                PostMessage(WM_QUIT, IntPtr.Zero, IntPtr.Zero);
+                thread.Join();
+                Win32ThreadID = 0;
+                thread = null;
+            }
+        }
+
+        bool IMessageFilter.PreFilterMessage(ref Message m)
+        {
+            CallbackDelegate handler;
+            if (MessageHandlers.TryGetValue(m.Msg, out handler))
+                handler(m);
+            return false;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool PostThreadMessage(int idThread, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int GetCurrentThreadId();
+    }
+
+#if EXAMPLE_CODE
+    class TEST
+    {
+        static void Main()
+        {
+            using (MessageThread thread = new MessageThread())
+            {
+                const int WM_COMMAND = 0x111;
+                thread.MessageHandlers[WM_COMMAND] = (m) => { MessageBox.Show(m.Msg.ToString()); };
+                thread.PostMessage(WM_COMMAND, IntPtr.Zero, IntPtr.Zero);
+                Console.ReadLine();
+            }
+        }
+    }
+#endif
+}
---
+++ Messaging/MessageWindow.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tsukikage.Windows.Messaging
+{
+    /// <summary>
+    /// Win32 native message window.
+    /// Win32 メッセージを受け取るためだけのウィンドウ
+    /// </summary>
+    [SuppressUnmanagedCodeSecurity]
+    public class MessageWindow : NativeWindow, IDisposable
+    {
+        public delegate void MessageHandler(Message m);
+
+        const int WM_CLOSE = 0x10;
+
+        /// <summary>
+        /// Install your message handler to this.
+        /// メッセージハンドラをこれに組み込む
+        /// </summary>
+        public Dictionary<int, MessageHandler> MessageHandlers { get; private set; }
+
+        /// <summary>
+        /// Create new message window thread.
+        /// 新しいメッセージウィンドウを作る
+        /// </summary>
+        public MessageWindow()
+        {
+            MessageHandlers = new Dictionary<int, MessageHandler>();
+            System.Windows.Forms.CreateParams cp = new System.Windows.Forms.CreateParams();
+            cp.ClassName = "Message";
+            CreateHandle(cp);
+        }
+
+        /// <summary>
+        /// Close window by posting WM_CLOSE and release all resouces.
+        /// WM_CLOSEを送ってウィンドウを閉じ、すべてのリソースを解放する。
+        /// </summary>
+        public void Dispose()
+        {
+            PostMessage(WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Post message to the window.
+        /// ウィンドウにメッセージを送る
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        public void PostMessage(int msg, IntPtr wParam, IntPtr lParam)
+        {
+            PostMessage(Handle, msg, wParam, lParam);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            MessageHandler handler;
+            if (MessageHandlers.TryGetValue(m.Msg, out handler))
+                handler(m);
+            else
+                base.WndProc(ref m);
+
+            if (m.Msg == WM_CLOSE)
+                DestroyHandle();
+        }
+        
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool PostMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+    }
+
+
+    class TEST
+    {
+        static void Main()
+        {
+            using (MessageWindow window = new MessageWindow())
+            {
+                const int WM_COMMAND = 0x111;
+                window.MessageHandlers[WM_COMMAND] = (m) => { MessageBox.Show(m.Msg.ToString()); };
+                window.PostMessage(WM_COMMAND, IntPtr.Zero, IntPtr.Zero);
+                Application.Run(new Form());
+            }
+        }
+    }
+}
